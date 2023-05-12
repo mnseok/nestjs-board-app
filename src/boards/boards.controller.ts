5e56eb523922c451f2f9c5460dc7f37ac5a8c815
mnseok <min.seok@g.skku.edu>
2023-05-12 10:14:22 +0900

    2강

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,7 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Get,Post,Body, Param, Delete,Patch } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
 import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
+
 @Controller('boards')
 export class BoardsController {
     constructor(private boardsService: BoardsService) {}
+
+    @Get()
+    getAllboards(): Board[] {
+        return this.boardsService.getAllBoards();
+    }
+    
+    @Get('/:id')
+    getBoardById(@Param('id') id : string): Board {
+        return this.boardsService.getBoardById(id);
+    }
+
+    @Post()
+    createBoard(@Body() CreateBoardDto: CreateBoardDto): Board {
+        return this.boardsService.createBoard(CreateBoardDto);
+    }
+
+    @Delete('/:id')
+    deleteBoard(@Param('id') id: string): void{
+        this.boardsService.deleteBoard(id);
+    }
+
+    @Patch('/:id/status')
+    updateBoardStatus(
+        @Param('id') id : string,
+        @Body('status') status: BoardStatus,
+    )   {
+        return this.boardsService.updateBoardStatus(id, status);
+    }
 
 }
